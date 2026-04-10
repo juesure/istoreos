bde740b26bb89e76bea7f01b5ef4263329d8666c
juesure <249899074@qq.com>
2026-04-10 14:51:48 +0800

    Update ipq807x.mk

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -363,7 +363,7 @@ define Device/qnap_301w
 endef
 TARGET_DEVICES += qnap_301w
 
-efine Device/redmi_ax6
+define Device/redmi_ax6
     $(call Device/xiaomi_ax3600)
     DEVICE_VENDOR := Redmi
     DEVICE_MODEL := AX6
